c447e3c79fda8e4061213f6a43cb6df4f41c4a1e
GabrielShaukan <gabrielshaukan@yahoo.com>
2020-02-13 19:28:56 +0100

    Added Search Insert Position Solution

---
+++ src/Problems/Problem35_SearchInsertPosition.java
@@ -0,0 +1,14 @@
+package Problems;
+
+public class Problem35_SearchInsertPosition {
+
+    public int solution(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+}
