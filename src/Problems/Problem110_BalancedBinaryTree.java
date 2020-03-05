1187ba0001933e9b3611466c8dce0989dbc6862e
Gabriel Shaukan <gabrielshaukan@yahoo.com>
2020-03-05 09:10:21 +0100

    Added Balanced Binary Tree Solution

---
+++ src/Problems/Problem110_BalancedBinaryTree.java
@@ -0,0 +1,19 @@
+package Problems;
+
+import utils.TreeNode;
+
+public class Problem110_BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        if (Math.abs(checkLevelMax(root.left) - checkLevelMax(root.right)) < 2)
+            return isBalanced(root.left) && isBalanced(root.right);
+        return false;
+    }
+
+    public int checkLevelMax(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(checkLevelMax(root.left)+1, checkLevelMax(root.right)+1);
+    }
+
+}
