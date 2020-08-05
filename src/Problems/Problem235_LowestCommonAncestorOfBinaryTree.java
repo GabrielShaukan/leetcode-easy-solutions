62977c5213ecf2e39bf62276cc4cdc73267e4bbf
Gabriel Shaukan <gabrielshaukan@yahoo.com>
2020-08-05 21:13:02 +0200

    Added Lowest Common Ancestor Binary Tree Solution

---
+++ src/Problems/Problem235_LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,13 @@
+package Problems;
+
+import Utils.TreeNode;
+
+public class Problem235_LowestCommonAncestorOfBinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left,p,q);
+        if(p.val > root.val && q.val >root.val) return lowestCommonAncestor(root.right,p,q);
+        else return root;
+    }
+}
